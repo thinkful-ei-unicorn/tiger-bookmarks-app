9cf7f90e2a529c1f7cc497479693c157de8146a6
tlhurd <tigerlillyhurd@gmail.com>
2020-11-29 19:12:16 -0500

    added item.js

---
+++ item.js
@@ -0,0 +1,12 @@
+const validateUrl = function (url) {
+  if (!url) throw new TypeError('URL must not be blank!');
+};
+
+const validateTitle = function (title) {
+  if (!title) throw new TypeError('Title must not be blank!');
+};
+
+export default {
+  validateUrl,
+  validateTitle
+}
